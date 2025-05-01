2555d7064fc0b0cb52f09bd2666b069a9c208f1e
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-05-01 22:58:40 +0100

    (feat) add placeholders, patterns, min & maxlength

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,21 @@ const label = document.querySelectorAll("label");
 for (value of label) {
     value.textContent = value.textContent.toUpperCase();
 }
+
+const userName = document.querySelectorAll(".name input")
+
+for (value of userName) {
+    value.setAttribute("maxlength", "10");
+    value.setAttribute("minlength", "1");
+}
+
+userName[0].setAttribute("placeholder", "Given Name");
+userName[1].setAttribute("placeholder", "Family Name");
+
+const email = document.querySelector("input[type='email']");
+email.setAttribute("placeholder", "your@example.com");
+
+const mobile = document.querySelector("input[type='tel']");
+mobile.setAttribute("placeholder", "xxx-xxx-xxxx");
+mobile.setAttribute("pattern", "[0-9]{3}[ -.][0-9]{3}[ -.][0-9]{4}");
+
